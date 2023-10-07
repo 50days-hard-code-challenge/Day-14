fb208ccbe955196448ce878202f9374522f3904c
YuvikaVarshney <130446237+YuvikaVarshney@users.noreply.github.com>
2023-10-08 01:13:16 +0530

    Update YuvikaVarshney2(zeroMatrix).py

--- YuvikaVarshney2(zeroMatrix).py
+++ YuvikaVarshney2(zeroMatrix).py
@@ -1,1 +1,21 @@
-matrix=[[0] for b in range(m)]
+def zeroMatrix(matrix, n, m):
+    # Write your code here.
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j]==0:
+                for k in range(n):
+                    if matrix[k][j]!=0:
+                        matrix[k][j]="x"
+                    
+                for k in range(m):
+                    if matrix[i][k]!=0:
+                        matrix[i][k]="x"
+
+                    
+
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j]=="x":
+                matrix[i][j]=0
+                
+    return matrix
